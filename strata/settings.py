573266673af987a9794f59af325d4b246f1a4f87
Matias Cocciolo <66866232+mattcocciolo@users.noreply.github.com>
2021-01-28 02:25:42 +0100

    Update settings.py

--- strata/settings.py
+++ strata/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
