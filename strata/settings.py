3c54fbfce44687651da10b9491e87c9099fbf1a7
Matias Cocciolo <66866232+mattcocciolo@users.noreply.github.com>
2021-01-28 15:36:07 +0100

    Update settings.py

--- strata/settings.py
+++ strata/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'matiasdjangosmtp@gmail.com'
-EMAIL_HOST_PASSWORD = 'matias3063240'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 
